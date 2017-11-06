c8bf9d117c9691706834b8871ecb354eb236a3f3
Mehrdad Shokri <code.poet95@gmail.com>
2017-11-06 21:29:34 +0330

    Extract build file into seperate directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+// In order to submit absolute path to webpack output.path property we require this node module, it's installed by default
+const path = require('path');
+
 // Webpack will try to parse this json just as you would do in you cli as an option
 module.exports = {
 	// The most important property required for webpack
@@ -7,6 +10,11 @@ module.exports = {
 	output: {
 		// All the additional code you see in bundle.js is to allow modules to be run in the browser.
 		// All of that code is called webpack runtime or the webpack bootstrap
-		filename: 'bundle.js'
-	}
+		filename: 'bundle.js',
+
+		//	we also can specify the build dir
+		//	it should be absolute path
+		path: path.join(__dirname, 'build')
+	},
+
 };
